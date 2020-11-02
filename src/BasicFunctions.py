d598b06690e31641312cacbf72ad29dc1688e72e
Alex Slivarić <raritacz@gmail.com>
2020-11-02 09:52:02 +0100

    My very first commit

--- src/BasicFunctions.py
+++ src/BasicFunctions.py
@@ -7,7 +7,7 @@ def add(num1, num2):
 
 # Function to subtract two numbers
 def subtract(num1, num2):
-    return NotImplemented
+    return num1 - num2
 
 
 # Function to multiply two numbers
@@ -17,6 +17,8 @@ def multiply(num1, num2):
 
 # Function to divide two numbers
 def divide(num1, num2):
-    return NotImplemented
+    return num1 : num2
+
+
 
 
